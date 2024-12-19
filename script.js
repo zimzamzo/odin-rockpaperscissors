df1ac960eea003dd383874f32f41354549e14885
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-19 16:52:06 -0600

    Created playRound and playGame

--- script.js
+++ script.js
@@ -30,3 +30,65 @@ function getHumanChoice() {
     return (choice === "r") ? "rock" : (choice === "p") ? "paper" : "scissors"
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice) {
+    let outcome;
+    let outcomeMsg;
+    switch(humanChoice) {
+        // human chose rock
+        case "rock":
+            outcome = (computerChoice==="rock") ? "tie" : (computerChoice==="paper") ? "lose" : "win";
+            break;
+        // human chose paper
+        case "paper":
+            outcome = (computerChoice==="rock") ? "win" : (computerChoice==="paper") ? "tie" : "lose";
+            break;
+        // human chose scissors
+        case "scissors":
+            outcome = (computerChoice==="rock") ? "lose" : (computerChoice==="paper") ? "win" : "tie";
+            break;
+    }
+
+    switch (outcome) {
+        case "win":
+            outcomeMsg = `Win! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
+            humanScore++;
+            break;
+
+        case "lose":
+            outcomeMsg = `Lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`;
+            computerScore++;
+            break;
+
+        case "tie":
+            outcomeMsg = "Tie! No winner this time!";
+            break;
+    }
+
+    console.log(outcomeMsg);
+}
+
+function playGame() {
+
+    let outcomeMsg;
+
+    for (let i = 0; i<5; i++) {
+        playRound(getHumanChoice(),getComputerChoice())
+    }
+    
+    if (humanScore > computerScore) {
+        outcomeMsg = "Rockstar! You defeated the computer.";
+    }
+    else if (humanScore < computerScore) {
+        outcomeMsg = "Sad times! You sorely lost to the computer.";
+    }
+    else {
+        outcomeMsg = "Well... That was a total tie. Better than losing?";
+    }
+
+    console.log(outcomeMsg);
+}
+
+playGame();
